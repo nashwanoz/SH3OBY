fc5000ed104e2fb65388f8ae1f038762d0b550d3
nashwanoz <t-3z@hotmail.com>
2026-08-11 02:40:55 +0300

    Update SmartLinkScaffold.kt

--- app/src/main/java/com/smartlink/app/ui/components/SmartLinkScaffold.kt
+++ app/src/main/java/com/smartlink/app/ui/components/SmartLinkScaffold.kt
@@ -30,6 +30,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.smartlink.app.ui.AppRoute
 
