3b52b6076566f48a99764840bd756aacac5ae954
nashwanoz <t-3z@hotmail.com>
2026-08-15 00:48:32 +0300

    Update PrintComponents.kt

--- app/src/main/java/com/khamrnet/app/ui/components/PrintComponents.kt
+++ app/src/main/java/com/khamrnet/app/ui/components/PrintComponents.kt
@@ -1,6 +1,4 @@
 package com.khamrnet.app.ui.components
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.foundation.rememberScrollState
 
 import android.Manifest
 import android.bluetooth.BluetoothDevice
@@ -15,7 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.verticalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
