d4e95e10b46abe733e3233f057bb87972319c69c
nashwanoz <t-3z@hotmail.com>
2026-08-15 00:33:28 +0300

    Update ProductsScreen.kt

--- app/src/main/java/com/khamrnet/app/ui/screens/ProductsScreen.kt
+++ app/src/main/java/com/khamrnet/app/ui/screens/ProductsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -69,7 +70,7 @@ fun ProductsScreen(state: AppUiState, viewModel: AppViewModel) {
         Spacer(Modifier.height(10.dp))
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxWidth().weight(1f),
             contentPadding = PaddingValues(bottom = 8.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
