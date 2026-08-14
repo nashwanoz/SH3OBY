f4d37befcfe4579084f766a103d9cea3a2eba060
nashwanoz <t-3z@hotmail.com>
2026-08-15 00:59:27 +0300

    Update TransfersScreen.kt

--- app/src/main/java/com/khamrnet/app/ui/screens/TransfersScreen.kt
+++ app/src/main/java/com/khamrnet/app/ui/screens/TransfersScreen.kt
@@ -1,10 +1,10 @@
 package com.khamrnet.app.ui.screens
-import androidx.compose.foundation.layout.fillMaxWidth
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
